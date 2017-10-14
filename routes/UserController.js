437a88f5da0ea09ed7964c41cecd7f14ece9e550
tombeau19 <thomas.beauregard@gmail.com>
2017-10-14 14:58:18 -0400

    index show new routes working for express

--- routes/UserController.js
+++ routes/UserController.js
@@ -2,10 +2,41 @@ const express = require('express')
 const { User } = require('../db/schema')
 const router = express.Router()
 
-
+//INDEX
 router.get('/', async (req, res) => {
     try {
         const users = await User.find({})
+        res.json(users)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+//SHOW
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+//NEW - CREATE
+router.post('/', async (req, res) => {
+    try {
+        const newUser = new User(req.body.user)
+        const saved = await newUser.save()
+        res.json(saved)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+//DELETE
+router.get('/:id/delete', async (req, res) => {
+    try {
+        const id = User.findById(req.params.id).remove()
         res.json(users)
     } catch (err) {
         res.send(err)
