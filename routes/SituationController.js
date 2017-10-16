bc9e0c31071dc0dc18323faf48b0b46c8b17806b
tombeau19 <thomas.beauregard@gmail.com>
2017-10-16 12:02:20 -0400

    new and update working for situation

--- routes/SituationController.js
+++ routes/SituationController.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { User, Setting, Sitution } = require('../db/schema')
+const { User, Setting, Situation } = require('../db/schema')
 const router = express.Router({ mergeParams: true })
 
 // INDEX ROUTE
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 router.get('/:situationId', async (req, res) => {
     try {
         const user = await User.findById(req.params.userId)
-        const settings = await user.settings.id(req.params.settingId) 
+        const settings = await user.settings.id(req.params.settingId)
         const situation = await settings.situations.id(req.params.situationId)
         res.json(situation)
     } catch (err) {
@@ -26,7 +26,45 @@ router.get('/:situationId', async (req, res) => {
     }
 })
 
-//
+// NEW - Empty Situation
+router.post('/', async (req, res) => {
+    try {
+        const newSituation = new Situation()
+        const user = await User.findById(req.params.userId)
+        const setting = await user.settings.id(req.params.settingId)
+        setting.situations.push(newSituation)
+        const saved = await user.save()
+        res.json(saved)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+//UPDATE
+router.patch('/:situationId', async (req, res) => {
+    try {
+        const updatedSituation = req.body.situation
+        const user = await User.findById(req.params.userId)
+        const setting = await user.settings.id(req.params.settingId)
+        const situation = setting.situations.id(req.params.situationId)
+        situation.title = updatedSituation.title
+        situation.description = updatedSituation.description
+        situation.response = updatedSituation.response
+        situation.comment = updatedSituation.comment
+        const saved = await user.save()
+        res.json(saved)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+// DELETE DELETE
+router.delete('/:situationId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId)
+        const setting = await user.settings.id(req.params.settingId)
+    }
+})
 
 
 
