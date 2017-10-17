c440492371d91c66fee7d9d57924573df089c6d8
tombeau19 <thomas.beauregard@gmail.com>
2017-10-17 18:21:36 -0400

    editable response added

--- client/src/components/Situations/Actions.jsx
+++ client/src/components/Situations/Actions.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Actions = (props) => {
 
+    const deleteSituation = () => {
+        props.deleteSituation(props._id)
+    }
+
+    const handleChange = (event) => {
+        props.handleChange(event, props._id)
+    }
+
+    const updateSituation = () => {
+        props.updateSituation(props._id)
+    }
+
         return (
             <div>
-                <li>{props.actions}</li>
+                <li><input  onBlur={updateSituation} onChange={handleChange} name="actions" value={props.actions}/></li>
             </div>
         );
 
